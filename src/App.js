8b8cfec877e793f545688ffe60c9754169fc6711
Ahmed-adel23 <121492522+Ahmed-adel23@users.noreply.github.com>
2026-04-16 17:25:47 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import Portofolio from './componnetns/porto.jsx';
+import 'bootstrap/dist/css/bootstrap.min.css';
+function App() {
+  return (
+    <Portofolio />
+  );
+}
+
+export default App;
